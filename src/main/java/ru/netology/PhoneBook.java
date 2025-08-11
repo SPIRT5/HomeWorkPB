68f2b379433ce1ef16e5f9d9f9cb06531200afca
SPIRT5 <arty-gordee@yandex.ru>
2025-08-11 19:14:31 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,13 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> nameToNumber = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        nameToNumber.putIfAbsent(name, number);
+        return nameToNumber.size();
     }
 }
